4082483c4ec09eff05bf447fed5d49afdc5ecc8a
grizz12 <raka_088@smkassalaambandung.sch.id>
2023-02-18 09:04:19 -0800

    fix fetch error

--- pages/surah/[surahNumber].js
+++ pages/surah/[surahNumber].js
@@ -1,27 +1,50 @@
-import { Heading, Text, VStack } from "@chakra-ui/react";
+import { Heading, Text, VStack, Box } from "@chakra-ui/react";
 import Head from "next/head";
 
 export default function SurahDetail({ surah }) {
-  const { nama, arti, ayat } = surah;
+  const { namaLatin, arti, ayat } = surah;
 
   return (
     <>
       <Head>
-        <title>{nama}</title>
-        <meta name="description" content={`Surah ${nama}`} />
+        <title>{namaLatin}</title>
+        <meta name="description" content={`Surah ${namaLatin}`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <VStack py={8} spacing={4}>
         <Heading as="h1" size="2xl">
-          {nama}
+          {namaLatin}
         </Heading>
         <Text fontSize="xl">{arti}</Text>
         {ayat.map((a) => (
-          <Text key={a.nomor} fontSize="xl">
-            {a.ar} ({a.nomor})
-          </Text>
+          //   <Text key={a.nomor} fontSize="xl">
+          //     {a.ar} ({a.teksArab})
+          //   </Text>
+          <Box
+            key={a.nomor}
+            display="flex"
+            alignItems="center"
+            py={2}
+            borderBottom="1px solid"
+            borderBottomColor="gray.200"
+          >
+            <Box w="20px" textAlign="right" fontWeight="bold" mr={2}>
+              {a.nomor}
+            </Box>
+            <Box>
+              <Text fontSize="xl" fontWeight="bold" textAlign={"justify"}>
+                {a.ar} {a.teksArab}
+              </Text>
+              <Text fontSize="lg" mt={1}>
+                {a.teksLatin}
+              </Text>
+              <Text fontSize="lg" mt={1}>
+                {a.teksIndonesia}
+              </Text>
+            </Box>
+          </Box>
         ))}
       </VStack>
     </>
